732687067f7479cb4566cc9139553ccf6dbae51f
unknown <farhanaji317@gmail.com>
2023-02-10 23:54:35 +0700

    add icons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../styles/profile.css'
-
+import CSS from '../images/css-3.png'
+import HTML from '../images/html-5.png'
+import JS from '../images/js.png'
+import ReactImg from '../images/react.png'
 
 const Profile = () => {
   return (
@@ -20,7 +23,9 @@ const Profile = () => {
                 </li>
                 <li>
                     <p>Flag:</p> 
-                    <span id='indo'></span>
+                    <a href='' title='INDONESIA'>
+                        <span id='indo'></span>
+                    </a>
                 </li>
                 <li>
                     <p>Desc:</p>
@@ -29,12 +34,21 @@ const Profile = () => {
             </ul>
         </div>
         <div className='skill'>
-            <h2>SKILL</h2>
-            <div className='skill-boxes'> 
-                <img className='skill-box'/>
-                <img className='skill-box'/>
-                <img className='skill-box'/>
-                <img className='skill-box'/>
+            <h2>SKILLS</h2>
+            <div className='skill-boxes'>
+                <a href="https://www.flaticon.com/free-icons/html-5" title="html 5 icons" target='_blank'>
+                    <img className='skill-box' src={HTML} alt='HTML 5 icons created by Freepik - Flaticon'/>
+                </a>
+                <a href="https://www.flaticon.com/free-icons/css-3" title="css 3 icons" target='_blank'>
+                    <img className='skill-box' src={CSS} alt='CSS icons created by Freepik - Flaticon'/>
+                </a>
+                <a href="https://www.flaticon.com/free-icons/javascript" title="javascript icons" target='_blank'>
+                    <img className='skill-box' src={JS} alt='Javascript icons created by Freepik - Flaticon'/>
+                </a>
+                <a href="https://www.flaticon.com/free-icons/javascript" title="javascript icons" target='_blank'>
+                    <img className='skill-box' src={JS} alt='Javascript icons created by Freepik - Flaticon'/>
+                </a>    
+                
             </div>
         </div>
     </div>
