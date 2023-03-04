8e3ff82ef5de2b31bee2fb4a6d2b3bcd6d8de9dc
farhanzip <farhanaji317@gmail.com>
2023-03-04 14:56:21 +0700

    use pure-react-carousel

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,58 +1,83 @@
-import React from 'react'
-import { useState } from 'react'
-import { useSpringCarousel, useTransitionCarousel } from 'react-spring-carousel'
-import '../styles/project.css'
-import projectArr from './projectsArray'
-import {BiLinkExternal} from 'react-icons/bi'
-import {FiGithub} from 'react-icons/fi'
+import React, { useContext, useState, useEffect } from "react";
+import {
+  ButtonBack,
+  ButtonNext,
+  CarouselContext,
+  CarouselProvider,
+  Dot,
+  Slider,
+  Slide,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import "../styles/project.css";
+import projectArr from "./projectsArray";
+import { BiLinkExternal } from "react-icons/bi";
+import { FiGithub } from "react-icons/fi";
 
-const Project = () => {
-  const [activeItem, setActiveItem] = useState(1)
-  const { 
-    carouselFragment,
-    useListenToCustomEvent,
-    slideToPrevItem, 
-    slideToNextItem 
-   } = useSpringCarousel({
-    withLoop: true,
-    items: projectArr.map((i) => ({
-      id: i.id,
-      renderItem: (
-        <div className='project-box'>
-           <a href={i.link} target="_blank">
-             <img src="" alt={i.altImg} />
-           </a>
-        </div>
-      ),
-    }))
-  });
+const ProjectCarousel = () => {
+  const carouselContext = useContext(CarouselContext);
+  const [currentSlide, setCurrentSlide] = useState(carouselContext.state.currentSlide);
 
-  useListenToCustomEvent((event) => {
-    if (event.eventName === "onSlideStartChange") {
-      setActiveItem(event.nextItem.id)
-    } 
-  });
+  useEffect(() => {
+    function onChange() {
+      setCurrentSlide(carouselContext.state.currentSlide);
+    }
+    carouselContext.subscribe(onChange);
+    return () => carouselContext.unsubscribe(onChange);
+  }, [carouselContext]);
+
+  const listProjects = projectArr.map((project, index) => (
+    <Slide index={index} key={index}>
+      <div className="project-card" >
+      <img key={project.id} src={project.src} alt={project.alt} />
+      </div>
+    </Slide>
+  ));
+
+  const listDot = projectArr.map((item,index) => (
+    <Dot slide={item.id} key={index} className="dot"></Dot>
+  ));
 
   return (
-    <div className='project-container'>
-      <h1 className='project-header'>PROJECTS</h1>
-      <h3>Some Cool Stuff I've built</h3>
-      <div className='carousel-wrapper'>
-        <div className='project-title'>{projectArr[activeItem].title}</div>
-        <div className='slide-counter'><span>{activeItem + 1} / {projectArr.length}</span></div>
-        {carouselFragment}
-        <div className='project-description'>{projectArr[activeItem].info}</div>
+    <div className="project-container">
+      <h1 className="project-header">PROJECTS</h1>
+      <h3>Some Stuff I've Built</h3>
+      <div className="project-title">
+          <p>{projectArr[currentSlide].title}</p>
       </div>
-      <div className='slide-wrapper'>
-          <button onClick={slideToPrevItem} className="btn-slide left"></button>
-          <a href='#' className='btn-visit' title='visit my project'><BiLinkExternal /></a>
-          <a href='#myProfile' className='btn-visit'> <FiGithub /></a>
-          
-          <button onClick={slideToNextItem} className="btn-slide right"></button>
+
+      <Slider>{listProjects}</Slider>
+
+      <div className="dot-wrapper">{listDot}</div>
+
+      <div className="slide-wrapper">
+        <ButtonBack className="btn-slide left"></ButtonBack>
+        <a href="#" className="btn-visit" title="visit my project">
+          <BiLinkExternal />
+        </a>
+        <a href="#myProfile" className="btn-visit">
+          <FiGithub />
+        </a>
+        <ButtonNext className="btn-slide right"></ButtonNext>
+      </div>
+        <div className="project-description">
+          <p id="slide-index">{projectArr[currentSlide].info}</p>
         </div>
-    </div>
+      </div>
+  );
+};
 
-  )
-}
+const Project = () => {
+  return (
+    <CarouselProvider
+      naturalSlideWidth={100}
+      naturalSlideHeight={90}
+      totalSlides={projectArr.length}
+      infinite={true}
+    >
+      <ProjectCarousel />
+    </CarouselProvider>
+  );
+};
 
-export default Project
+export default Project;
--- src/components/projectsArray.js
+++ src/components/projectsArray.js
@@ -1,65 +1,28 @@
 const projectArr = [
 {
-   "id" : 0,
-   "title" : "project 1",
-   "link" : "www.google.com",
-   "img" : " ",
-   "altImg" : "portofolio 1",
-   "info" : "project 1 is lorem ipsum"
+   id : 0,
+   title : "ANIQUOTE",
+   link : "http://react-quote.vercel.app/",
+   src : require('../images/portofolio/aniquote.png').default,
+   alt : "aniquote",
+   info : "ANIQUOTE is random anime quote generator, You can get motivational quote for better live (maybe)"
 },
 {
-    "id" : 1,
-    "title" : "project 2",
-    "link" : "www.google.com",
-    "img" : " ",
-    "altImg" : "portofolio 2",
-    "info" : "project 2 is lorem ipsum"
- },
+   id : 1,
+   title : "PROJECT 2",
+   link : "http://react-quote.vercel.app/",
+   src : require('../images/portofolio/aniquote.png').default,
+   alt : "project2",
+   info : "Project 2 Incoming (maybe)"
+},
  {
-    "id" : 2,
-    "title" : "project 3",
-    "link" : "www.google.com",
-    "img" : " ",
-    "altImg" : "portofolio 3",
-    "info" : "project 3 is lorem ipsum"
- }
+   id : 2,
+   title : "PROJECT3",
+   link : "http://react-quote.vercel.app/",
+   src : require('../images/portofolio/aniquote.png').default,
+   alt : "aniquote",
+   info : "Still thinking project"
+}
 ]
-// [
-//     {
-//       id: 'item-1',
-//       renderItem: (
-//         <div className='project-box'>
-//           <h2>ANIQUOTE</h2>
-//           <a href="www.google.com">
-//             <img src="" alt="portofolio 1" />
-//           </a>
-//           <p>This project using API for generate random quote</p>
-//         </div>
-//       )
-//     },
-//     {
-//       id: 'item-2',
-//       renderItem: (
-//         <div className='project-box'>
-//           <h2>KOTAKODE CLONE</h2>
-//           <a href="www.google.com">
-//             <img src="" alt="portofolio 2 " />
-//           </a>
-//           <p>Landing Page for kotakode using bootstrap and react</p>
-//         </div>
-//       )
-//     },
-//     {
-//       id: 'item-3',
-//       renderItem: (
-//         <div className='project-box'>
-//           <h2>RANDOM PORTO</h2>
-//           <a href="www.google.com">
-//             <img src="" alt="portofolio 3" />
-//           </a>
-//           <p>Random portofolio Lorem ipsum dolor sit amet consectetur.</p>
-//         </div>
-//       )
-//     },
-//   ]
+
 export default projectArr
